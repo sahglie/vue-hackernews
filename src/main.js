a3adf4658df383abd140457cfd8ec78d7529256e
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-09-23 10:16:33 +0100

    refactor: add /api to project

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import {
+  fetchItems,
+  fetchIdsByType
+} from './api/api'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<app/>',
-  components: { App }
+function getTopItems () {
+  return new Promise(resolve => { // eslint-disable-line no-new
+    fetchIdsByType('top')
+    .then(ids => fetchItems(ids))
+    .then(items => resolve(items))
+  })
+}
+
+getTopItems().then(res => {
+  window.items = res
+  new Vue({ // eslint-disable-line no-new
+    el: '#app',
+    render: h => h(App)
+  })
 })
